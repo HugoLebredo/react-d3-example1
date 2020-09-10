284cc8fe2dd018133640223b8e9c0cd20e7ee8fa
HugoLebredo <hugolebredo@gmail.com>
2020-09-10 20:45:53 +0200

    Updates

---
+++ precache-manifest.63c36ddfa6562dbb8367cca3efa37301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d1c22cf81856ea79b45c8fef1149dd",
+    "url": "/react-d3-example1/index.html"
+  },
+  {
+    "revision": "fe92e8b6b0ce6bcf9baa",
+    "url": "/react-d3-example1/static/css/main.99845c8f.chunk.css"
+  },
+  {
+    "revision": "949d269aca4dce890483",
+    "url": "/react-d3-example1/static/js/2.e9009e43.chunk.js"
+  },
+  {
+    "revision": "2aa452a43c496865ee4e2291133adeed",
+    "url": "/react-d3-example1/static/js/2.e9009e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe92e8b6b0ce6bcf9baa",
+    "url": "/react-d3-example1/static/js/main.a735904c.chunk.js"
+  },
+  {
+    "revision": "62eb4bbc93e0c9c3cd9e",
+    "url": "/react-d3-example1/static/js/runtime-main.829f8db2.js"
+  }
+]);
