a90c5eb501d74ddf205a8a57b0d5b2703a807b13
HugoLebredo <hugolebredo@gmail.com>
2020-09-10 20:52:35 +0200

    Commands deploy

--- src/services/transformTest.js
+++ src/services/transformTest.js
@@ -1,45 +1,43 @@
 import _, { flatten, keyBy } from 'lodash';
 import * as d3 from 'd3';
 
-import { width, height, margin, daysOfTheWeek } from '../data/config';
+import { width, height, marginq } from '../data/config';
 
-const xScale = d3.scaleBand().domain(_.map(daysOfTheWeek,0))
+const xScale = d3.scaleLinear().domain([0, 6])
                              .range([margin.left, width - margin.right]);
-                           // .range([margin.left, width - margin.right]);
 
 var selectedWeekRadius = (width - margin.left - margin.right) / 2;
 
 const trasformTest = (data,selectedWeek) => {
-    console.log(data);
+
     var weeksExtent = d3.extent(Object.keys(data),d => new Date(d))
     
     const yScale = d3.scaleLinear().domain(weeksExtent)
                                     .range([height - margin.bottom, margin.top]);
 
     const UltimateData =_.chain(data)
-    .map((week,key) => {
-        selectedWeek = new Date(selectedWeek)
-        key = new Date(key)
-        return _.map(week,d => {
-            var focusX = xScale(d.WeekDay);
-            var focusY = yScale(new Date(key)) + height;
-                
-            if (key.getTime() === selectedWeek.getTime()) {
+        .map((week,key) => {
+            selectedWeek = new Date(selectedWeek)
+            key = new Date(key)
+            return _.map(week,d => {
+                var focusX = xScale(d.WeekDay);
+                var focusY = yScale(new Date(key)) + height;
+                    
+                if (key.getTime() === selectedWeek.getTime()) {
 
-                var perAngle = Math.PI/6;
-                var angle = Math.PI - perAngle * d.WeekDay;
-                focusX = selectedWeekRadius * Math.cos(angle) + selectedWeekRadius + margin.left
-                focusY = selectedWeekRadius * Math.sin(angle) + margin.top
-        }
-            return Object.assign(d ,{
-                focusX,
-                focusY,
-
+                    var perAngle = Math.PI/6;
+                    var angle = Math.PI - perAngle * d.WeekDay;
+                    focusX = selectedWeekRadius * Math.cos(angle) + selectedWeekRadius + margin.left
+                    focusY = selectedWeekRadius * Math.sin(angle) + margin.top
+            }
+                return Object.assign(d ,{
+                    focusX,
+                    focusY,
+                })
             })
-        })
-    }).flatten().value()
+        }).flatten().value()
 
-    return(UltimateData);
+        return(UltimateData);
 }
     
 export default trasformTest
